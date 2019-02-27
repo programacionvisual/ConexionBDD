f1671298f35f494a0f5308267a6c97f2ffdaf677
wssolis <sys.solis@gmail.com>
2019-02-26 22:26:55 -0500

    Version Ssolis 2

--- Practica_18022019/src/ec/edu/intsuperior/controlador/Aplicacion.java
+++ Practica_18022019/src/ec/edu/intsuperior/controlador/Aplicacion.java
@@ -16,6 +16,7 @@ import javax.swing.JOptionPane;
  */
 public class Aplicacion {
     public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "No deseesperen");
        Controlador controlador = new Controlador();
        controlador.mostrarLogin();
        
